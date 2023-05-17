ca309423048c40bcfecdd20a24d9cbb94c415872
erik <erikpersson0884@gmail.com>
2023-05-17 17:02:16 +0200

    started on the product list item class

--- imat/MainViewController.java
+++ imat/MainViewController.java
@@ -2,14 +2,19 @@
 package imat;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
+import se.chalmers.cse.dat216.project.Product;
 
 public class MainViewController implements Initializable {
+
+    @FXML FlowPane productListFlowPane;
 
 
     IMatDataHandler iMatDataHandler = IMatDataHandler.getInstance();
@@ -17,7 +22,15 @@ public class MainViewController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
 
         String iMatDirectory = iMatDataHandler.imatDirectory();
+        updateProductList();
+    }
 
+
+
+    private void updateProductList(){
+        productListFlowPane.getChildren().clear();
+        for (Product product : iMatDataHandler.getProducts())
+            productListFlowPane.getChildren().add(product);
     }
 
 }
