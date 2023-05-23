f3bf58687e2793ceeb7b540611a2d0fdd777a35a
erik <erikpersson0884@gmail.com>
2023-05-23 11:21:29 +0200

    added navigation to main page

--- imat/iMatApp.java
+++ imat/iMatApp.java
@@ -1,11 +1,18 @@
 package imat;
 
 
+import java.io.IOException;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 /*
@@ -14,7 +21,9 @@ import javafx.stage.Stage;
  * and open the template in the editor.
  */
 public class iMatApp extends Application {
-    
+
+    @FXML Button nextButton;
+
     @Override
     public void start(Stage stage) throws Exception {
         
@@ -22,13 +31,25 @@ public class iMatApp extends Application {
         
         Parent root = FXMLLoader.load(getClass().getResource("imat_app.fxml"), bundle);
         
-        Scene scene = new Scene(root, 1440, 1024);
+        Scene scene = new Scene(root, 1280, 720);
         
         stage.setTitle(bundle.getString("application.name"));
         stage.setScene(scene);
         stage.show();
 
     }
+
+
+    //===============================================
+
+
+
+
+
+
+
+
+// ===========================================
 
     /**
      * @param args the command line arguments
